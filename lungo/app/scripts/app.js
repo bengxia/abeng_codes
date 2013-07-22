9d0f326d4dfb8fffce3f6346d216752f2ce27bda
abeng <bengxia@gmail.com>
2013-07-22 17:12:01 +0800

    add lungo sub-project, yo express-angular

---
+++ lungo/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('lungoApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ lungo/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lungoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
